8490139682ff68a29178429dbdae1175aff07f46
Jesse Yang <jyyjcc@gmail.com>
2013-09-06 19:17:15 +0800

    add test cases for waitRule and dialog

--- test/webot.js
+++ test/webot.js
@@ -34,7 +34,7 @@ describe('webot', function() {
     robot.reply(info, callback);
   }
 
-  describe('set rule', function() {
+  describe('set', function() {
     it('should raise exception when no arguments', function() {
       var err;
       try { robot.set(); } catch (e) { err = e; }
@@ -74,9 +74,198 @@ describe('webot', function() {
     });
   });
 
+  describe('dialog', function() {
+    var file = __dirname + '/dialog.json';
+    it('should load all', function() {
+      robot.dialog(file);
+      should.exist(robot.get('dialog_haha_1'));
+      should.exist(robot.get('dialog_haha_2'));
+    });
+    it('should pick one from list', function(done) {
+      reply('haha_1', function(err, info) {
+        require(file)['haha_1'].should.include(info.reply);
+        done();
+      });
+    });
+    it('should replace match', function(done) {
+      reply('match ABC', function(err, info) {
+        should.equal(info.reply, 'ABC');
+        done();
+      });
+    });
+    it('should replace match2', function(done) {
+      reply('match2 ABC', function(err, info) {
+        should.equal(info.reply, 'return ABC');
+        done();
+      });
+    });
+  });
+
+
+  describe('get', function() {
+    it('can get named rule', function() {
+      should.exist(robot.get('named'));
+    });
+    it('should return undefined when nothing', function() {
+      should.strictEqual(robot.get('nothing'), undefined);
+    });
+  });
+
+  describe('wait', function() {
+    var info, session;
+
+    before(function() {
+      session = {};
+    });
+    beforeEach(function() {
+      info = new webot.Info();
+      info.session = session;
+    });
+
+    it('should handle replies', function(done) {
+      robot.set('dial', 'choose: 1. A, 2. B, 3. C', {
+        '=1': 'A',
+        '=2': 'B',
+        '=3': 'C',
+        '/.*/': function(info) {
+          info.rewait();
+          return 'wrong choice, please input one of 1/2/3';
+        }
+      });
+      info.text = 'dial';
+      reply(info, function(err, info) {
+        info.reply.should.include('choose');
+        done();
+      });
+    });
+    it('should choose dial', function(done) {
+      info.text = '1';
+      reply(info, function(err, info) {
+        should.equal(info.reply, 'A');
+        done();
+      });
+    });
+    it('could run again', function(done) {
+      info.text = 'dial';
+      reply(info, function(err, info) {
+        info.reply.should.include('choose');
+        done();
+      });
+    });
+    it('should go into rewait', function(done) {
+      info.text = 'not 1,2,3';
+      reply(info, function(err, info) {
+        info.reply.should.include('wrong choice');
+        done();
+      });
+    });
+    it('should do rewait', function(done) {
+      info.text = '2';
+      reply(info, function(err, info) {
+        should.equal(info.reply, 'B');
+        done();
+      });
+    });
+
+    it('should allow waitRule function', function(done) {
+      robot.set('go wait_1', function(info) {
+        info.session.wait_1_data = 'me';
+        info.wait('wait_1');
+        return 'ok';
+      });
+      robot.waitRule('wait_1', function(info) {
+        return 'waited ' + info.session.wait_1_data;
+      });
+      info.text = 'go wait_1';
+      reply(info, function(err, info) {
+        should.equal(info.reply, 'ok');
+        done();
+      });
+    });
+
+    it('should raise exception for wait rule name conflicts', function() {
+      var err;
+      try { robot.waitRule('wait_1', function(){ }); } catch (e) { err = e; }
+      should.exist(err);
+    });
+
+    it('should do wait_1', function(done) {
+      info.text = 'whatever';
+      reply(info, function(err, info) {
+        should.equal(info.reply, 'waited me');
+        done();
+      });
+    });
+
+    it('should pass waitRule as getter', function() {
+      should.exist(robot.waitRule('wait_1'));
+      // hidden reply rules
+      should.exist(robot.waitRule('_reply_/dial/'));
+    });
+
+    it('should allow waitRule object', function(done) {
+      robot.set('your sex', function(info) {
+        info.wait('guess sex');
+        return 'you guess!';
+      });
+      robot.waitRule('guess sex', {
+        '=female': 'wrong',
+        '=male': 'right',
+        '/.*/': function(info) {
+          info.rewait();
+          return 'guess again';
+        }
+      });
+      info.text = 'your sex';
+      reply(info, function(err, info) {
+        should.equal(info.reply, 'you guess!');
+        done();
+      });
+    });
+
+    it('should reply waitRule', function(done) {
+      info.text = 'male';
+      reply(info, function(err, info) {
+        should.equal(info.reply, 'right');
+        done();
+      });
+    });
+
+  });
+
+  describe('reply', function() {
+    it('should handle plain object', function(done) {
+      robot.reply({ text: 'test' }, function(err, info) {
+        should.not.exist(err);
+        should.exist(info.reply);
+        done();
+      });
+    });
+    it('should handle Info request', function(done) {
+      var req = webot.Info({ text: 'test' });
+      reply(req, function(err, info) {
+        should.not.exist(err);
+        should.exist(info.reply);
+        should.equal(req, info);
+        done();
+      });
+    });
+    it('should handle func', function(done) {
+      reply('func', function(err, info) {
+        info.reply.should.include('func');
+        done();
+      });
+    });
+    it('should pass strict equal', function(done) {
+      async.map(['strict', 'not strict'], reply, function(err, results) {
+        results[0].reply.should.include('strict');
+        results[1].reply.should.not.include('strict');
+        done();
+      });
+    });
+  });
 
   describe('beforeReply', function() {
-
     it('should do before reply', function(done) {
       robot.set({
         pattern: 'test before',
@@ -129,38 +318,5 @@ describe('webot', function() {
       });
     });
   });
-
-  describe('reply', function() {
-    it('should handle plain object', function(done) {
-      robot.reply({ text: 'test' }, function(err, info) {
-        should.not.exist(err);
-        should.exist(info.reply);
-        done();
-      });
-    });
-    it('should handle Info request', function(done) {
-      var req = webot.Info({ text: 'test' });
-      reply(req, function(err, info) {
-        should.not.exist(err);
-        should.exist(info.reply);
-        should.equal(req, info);
-        done();
-      });
-    });
-    it('should handle func', function(done) {
-      reply('func', function(err, info) {
-        info.reply.should.include('func');
-        done();
-      });
-    });
-    it('should pass strict equal', function(done) {
-      async.map(['strict', 'not strict'], reply, function(err, results) {
-        results[0].reply.should.include('strict');
-        results[1].reply.should.not.include('strict');
-        done();
-      });
-    });
-  });
-
 
 });
